da936a15018fd795d98f6e81a906545db8434ac5
janradzik86 <87680038+janradzik86@users.noreply.github.com>
2026-09-23 16:13:53 +0200

    app-a: prefer inline bank-data, fallback texts.json

--- app-a.js
+++ app-a.js
@@ -2,10 +2,15 @@
   (function(){
     var _bank = {version:1, entries:[]};
     try {
-      var xhr = new XMLHttpRequest();
-      xhr.open('GET', 'texts.json', false);
-      xhr.send(null);
-      if (xhr.status >= 200 && xhr.status < 300) _bank = JSON.parse(xhr.responseText);
+      var el = document.getElementById('bank-data');
+      if (el && el.textContent) {
+        _bank = JSON.parse(el.textContent);
+      } else {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', 'texts.json', false);
+        xhr.send(null);
+        if (xhr.status >= 200 && xhr.status < 300) _bank = JSON.parse(xhr.responseText);
+      }
     } catch (e) {}
     window.__HOT_BANK__ = _bank;
   })();
